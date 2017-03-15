2c43ec292275b0cc75648f377c4dcb9958fb972c
tinogomes <tinorj@gmail.com>
2017-03-15 11:13:58 -0300

    adding platform on header request

--- lib/intelipost/client.rb
+++ lib/intelipost/client.rb
@@ -1,26 +1,27 @@
-require 'intelipost/mash'
+require "intelipost/mash"
 
 module Intelipost
   class Client
-    HOST        = 'api.intelipost.com.br'
-    SERVICE     = 'api/'
-    API_VERSION = 'v1'
-    attr_reader :uri, :options, :connection, :api_key
+    HOST        = "api.intelipost.com.br"
+    SERVICE     = "api/"
+    API_VERSION = "v1"
+    attr_reader :uri, :options, :connection, :api_key, :platform
 
     def initialize(options)
-      @options = {ssl: true}
+      @options = { ssl: true }
       @options.merge! options
-      uri = @options[:ssl] ? 'https://' : 'http://'
+      uri = @options[:ssl] ? "https://" : "http://"
       uri.concat HOST
 
       @uri = URI.join uri, SERVICE, API_VERSION
       @api_key = @options[:api_key]
+      @platform = @options[:platform]
 
       @connection = connection
     end
 
     def connection
-      raise ArgumentError, 'an api_key is required to connect' if api_key.nil?
+      raise ArgumentError, "an api_key is required to connect" if api_key.nil?
 
       Faraday::Middleware.register_middleware gzip: lambda { FaradayMiddleware::Gzip }
       Faraday.new(url: @uri.to_s) do |conn|
@@ -29,7 +30,8 @@ module Intelipost
         conn.response :mashify, { mash_class: Intelipost::Mash }
         conn.response :json
 
-        conn.headers['api_key'] = api_key
+        conn.headers["api_key"] = api_key
+        conn.headers["platform"] = platform unless platform.nil?
         conn.adapter :net_http
         conn.use :gzip
         conn.proxy @options[:proxy]
@@ -57,7 +59,7 @@ module Intelipost
       string = term.to_s
       string = string.sub(/^[a-z\d]*/) { $&.capitalize }
       string.gsub!(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{$2.capitalize}" }
-      string.gsub!('/', '::')
+      string.gsub!("/", "::")
       string
     end
   end
--- spec/lib/intelipost/client_spec.rb
+++ spec/lib/intelipost/client_spec.rb
@@ -1,103 +1,125 @@
-# coding: utf-8
+# frozen_string_literal: true
 
 describe Intelipost::Client, :vcr do
-  subject { Intelipost::Client.new api_key: ENV['INTELIPOST_API_KEY'] }
+  subject { described_class.new api_key: ENV["INTELIPOST_API_KEY"], platform: "intelipost-gem-test" }
 
-  context 'when initialized with no options' do
-    subject { Intelipost::Client }
+  context "when initialized with no options" do
+    subject { described_class }
 
-    it 'raises ArgumentError if initialized with no options' do
+    it "raises ArgumentError if initialized with no options" do
       expect do
-        Intelipost::Client.new
+        described_class.new
       end.to raise_error ArgumentError
     end
 
-    it 'raises ArgumentError if initialized without credentials' do
+    it "raises ArgumentError if initialized without credentials" do
       expect do
-        Intelipost::Client.new api_key: nil
+        described_class.new api_key: nil
       end.to raise_error ArgumentError
     end
   end
 
-  context 'when call for an non existing defined class' do
-    it '.method_missing' do
+  context "when call for an non existing defined class" do
+    it ".method_missing" do
       expect { subject.foobar(1) }.to raise_error ArgumentError
     end
   end
 
-  context 'initialized with credentials' do
-    describe '#connection' do
-      it 'is an instance of Faraday::Connection' do
+  context "without platform" do
+    subject { described_class.new api_key: ENV["INTELIPOST_API_KEY"] }
+
+    describe "#connection" do
+      it "is an instance of Faraday::Connection" do
         expect(subject.connection).to be_an_instance_of Faraday::Connection
       end
 
-      it 'has the api_key header on the request' do
-        expect(subject.connection.headers).to include('api_key')
+      it "has the api_key header on the request" do
+        expect(subject.connection.headers).to include("api_key")
+      end
+
+      it "has not the platform header on the request" do
+        expect(subject.connection.headers).not_to include("platform")
       end
     end
   end
 
-  context 'dealing with options' do
-    it 'includes api_key' do
-      expect(subject.options).to include(:api_key)
+  context "initialized with credentials" do
+    describe "#connection" do
+      it "is an instance of Faraday::Connection" do
+        expect(subject.connection).to be_an_instance_of Faraday::Connection
+      end
+
+      it "has the api_key header on the request" do
+        expect(subject.connection.headers).to include("api_key")
+      end
+
+      it "has the platform header on the request" do
+        expect(subject.connection.headers).to include("platform")
+      end
     end
   end
 
-  context 'dealing with zipcode (cep)' do
-    it 'returns a Intelipost::Mash on successful query' do
-      response = subject.cep.address_complete.get('04661100')
+  context "dealing with options" do
+    it "includes api_key" do
+      expect(subject.options).to include(:api_key)
+    end
+
+    it "includes platform" do
+      expect(subject.options).to include(:platform)
+    end
+  end
+
+  context "dealing with zipcode (cep)" do
+    it "returns a Intelipost::Mash on successful query" do
+      response = subject.cep.address_complete.get("04661100")
       expect(response.class).to eq Intelipost::Mash
       expect(response.success?).to eq true
       expect(response.failure?).to eq false
-      expect(response.all_messages).to eq ''
+      expect(response.all_messages).to eq ""
       expect(response.messages).to be_empty
     end
   end
 
-  context 'dealing with Quotes' do
+  context "dealing with Quotes" do
     let(:volumes) do
       {
-        'origin_zip_code' => '04037-003',
-        'destination_zip_code' => '06396-200',
-        'volumes' => [
+        "origin_zip_code" => "04037-003",
+        "destination_zip_code" => "06396-200",
+        "volumes" => [
           {
-            'weight' => 0.1,
-            'volume_type' => 'BOX',
-            'cost_of_goods' => 100,
-            'width' => 10,
-            'height' => 10,
-            'length' => 10
+            "weight" => 0.1,
+            "volume_type" => "BOX",
+            "cost_of_goods" => 100,
+            "width" => 10,
+            "height" => 10,
+            "length" => 10
           }
         ],
-        'additional_information' => {
-          'free_shipping' => false,
-          'extra_cost_absolute' => 0,
-          'extra_cost_percentage' => 0,
-          'lead_time_business_days' => 0,
-          'sales_channel' => 'hotsite',
-          'client_type' => 'gold',
-          'delivery_method_ids' => [
-          4,
-          3,
-          2
-          ]
+        "additional_information" => {
+          "free_shipping" => false,
+          "extra_cost_absolute" => 0,
+          "extra_cost_percentage" => 0,
+          "lead_time_business_days" => 0,
+          "sales_channel" => "hotsite",
+          "client_type" => "gold",
+          "delivery_method_ids" => [ 4, 3, 2 ]
         }
       }
     end
 
-    it '.quote.create' do
+    it ".quote.create" do
       expect(subject.quote.create(volumes)).to have_key(:content)
     end
 
-    it '.quote.create.failure' do
-      response = subject.quote.create({'failure'=>'failure'})
+    it ".quote.create.failure" do
+      response = subject.quote.create({"failure"=>"failure"})
       expect(response.success?).to eq false
       expect(response.failure?).to eq true
-      expect(response.all_messages).not_to eq ''
+      expect(response.all_messages).not_to eq ""
       expect(response.messages).not_to be_empty
     end
 
-    it '.quote.get(#)' do
+    it ".quote.get(#)" do
       quote_id = subject.quote.create(volumes).content.id
       expect(subject.quote.get(quote_id)).to have_key(:content)
     end
